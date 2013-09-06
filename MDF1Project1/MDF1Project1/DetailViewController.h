e17616d33a083f6197aea240ec61cbe1a0a91e32
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-06 17:46:33 -0600

    Added detail view and added logic to put item details in labels

---
+++ MDF1Project1/MDF1Project1/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewViewController.h
+//  MDF1Project1
+//
+//  Created by Elijah Freestone on 9/6/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewViewController : UIViewController
+
+@end
---
+++ MDF1Project1/MDF1Project1/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewViewController.m
+//  MDF1Project1
+//
+//  Created by Elijah Freestone on 9/6/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "DetailViewViewController.h"
+
+@interface DetailViewViewController ()
+
+@end
+
+@implementation DetailViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
