4bdb540bc1347414d0bcf24d9eedd56346452c23
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-06 12:37:29 -0600

    Added custom table cell and tweaked code accordingly. Not loading names from arrays yet

---
+++ MDF1Project1/MDF1Project1/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  MDF1Project1
+//
+//  Created by Elijah Freestone on 9/6/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
