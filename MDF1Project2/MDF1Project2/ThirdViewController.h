a07b41ff427fd8efb9f1cc4fbb7037a0ed87df98
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-09 20:59:03 -0600

    Added third tab/view. Currently using first tabs icon

---
+++ MDF1Project2/MDF1Project2/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/9/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ MDF1Project2/MDF1Project2/ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/9/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
