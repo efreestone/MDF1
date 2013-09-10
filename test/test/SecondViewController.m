62fd21746b2b025fa648c92ef7ccba5d9411180e
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-09 20:10:48 -0600

    Removed project 2 again to rebuild with single nib for simplicity

---
+++ test/test/SecondViewController.m
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.m
+//  test
+//
+//  Created by Elijah Freestone on 9/9/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Second", @"Second");
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
