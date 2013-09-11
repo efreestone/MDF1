e10004522d02fd444e95a0ff951c20d3a6e875e9
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-10 21:47:37 -0600

    Created Custom table view cell. Still working to get it displaying

---
+++ MDF1Project2/MDF1Project2/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/10/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UIViewController
+
+@end
---
+++ MDF1Project2/MDF1Project2/CustomTableViewCell.m
@@ -0,0 +1,38 @@
+//
+//  CustomTableViewCell.m
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/10/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@interface CustomTableViewCell ()
+
+@end
+
+@implementation CustomTableViewCell
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
