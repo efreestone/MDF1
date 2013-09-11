bfc1ea34c660c0dbd3b7146a53067d61ad25ec03
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-10 20:58:18 -0600

    Added detail view and started copy and pasting code from project 1 for albums table view. Still need to make custom cell

---
+++ MDF1Project2/MDF1Project2/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/10/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
