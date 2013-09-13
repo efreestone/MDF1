d51eb6135e6e55cb7d68f66cc25dcbc6267bcd2c
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-12 19:52:00 -0600

    Created research view and have displaying ok. Starting on bio view

---
+++ MDF1Project2/MDF1Project2/ResearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResearchViewController.h
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/12/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResearchViewController : UIViewController
+
+@end
---
+++ MDF1Project2/MDF1Project2/ResearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResearchViewController.m
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/12/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "ResearchViewController.h"
+
+@interface ResearchViewController ()
+
+@end
+
+@implementation ResearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
