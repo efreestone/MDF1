ed112eda2fe07585514a09c0d2371ab356c4521f
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-12 20:05:18 -0600

    Added bio view with controller. Filled in static for now. Time to start my video

---
+++ MDF1Project2/MDF1Project2/BioViewController.m
@@ -0,0 +1,38 @@
+//
+//  BioViewController.m
+//  MDF1Project2
+//
+//  Created by Elijah Freestone on 9/12/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "BioViewController.h"
+
+@interface BioViewController ()
+
+@end
+
+@implementation BioViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
