f85d92a156527f215690796f95fc7821763ad4a7
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-17 22:35:27 -0600

    Created Location Manager to store location info and create singleton. Still working on getting it passed and displaying

---
+++ MDF1Project3/MDF1Project3/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  MDF1Project3
+//
+//  Created by Elijah Freestone on 9/17/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ MDF1Project3/MDF1Project3/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  MDF1Project3
+//
+//  Created by Elijah Freestone on 9/17/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
