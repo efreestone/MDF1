77b13eba6392115b72ae988830a24ac917a3a8dd
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-19 13:05:00 -0600

    Added maps to both second views and started working on annotations

---
+++ MDF1Project3/MDF1Project3/MyMapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  MyMapAnnotation.h
+//  MDF1Project3
+//
+//  Created by Elijah Freestone on 9/19/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyMapAnnotation : NSObject
+
+@end
---
+++ MDF1Project3/MDF1Project3/MyMapAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MyMapAnnotation.m
+//  MDF1Project3
+//
+//  Created by Elijah Freestone on 9/19/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "MyMapAnnotation.h"
+
+@implementation MyMapAnnotation
+
+@end
