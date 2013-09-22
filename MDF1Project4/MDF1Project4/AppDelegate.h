3ef4bd6d03b1677ef783f1fbe1435e7cad0c701b
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-21 19:51:01 -0600

    Initial commit for project 4 with new tabbed app

---
+++ MDF1Project4/MDF1Project4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDF1Project4
+//
+//  Created by Elijah Freestone on 9/21/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ MDF1Project4/MDF1Project4/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MDF1Project4
+//
+//  Created by Elijah Freestone on 9/21/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
