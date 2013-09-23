3a458965415849ba04e46ec98c19e7fa787b6763
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-23 17:04:34 -0600

    Set up basic framework for project with table and detail view (with nib) on first, and text view on second tab

---
+++ MDF1Project4/MDF1Project4/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MDF1Project4
+//
+//  Created by Elijah Freestone on 9/23/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ MDF1Project4/MDF1Project4/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.m
+//  MDF1Project4
+//
+//  Created by Elijah Freestone on 9/23/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
