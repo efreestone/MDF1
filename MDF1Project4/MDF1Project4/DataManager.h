27c932f01e64f18c16b871cd9f5d6d257ee0ca96
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-09-25 22:50:28 -0600

    Added data manager. Working on xml parse still. Making some progress but still not working right

---
+++ MDF1Project4/MDF1Project4/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  MDF1Project4
+//
+//  Created by Elijah Freestone on 9/25/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+@end
---
+++ MDF1Project4/MDF1Project4/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  MDF1Project4
+//
+//  Created by Elijah Freestone on 9/25/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
